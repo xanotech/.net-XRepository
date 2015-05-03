68a56b2c92ce0426c500ec63eaf50868ce8f2f8b
Aron Boyette <aron.boyette@xanotech.com>
2015-05-03 11:42:20 -0400

    Enhanced Cursor.Sort by adding additional overloads: one for accepting an IDictionary<string, bool> and another for accepting any object where the object's properties can be used as column specifiers and the property values are used as indicating direction.

--- Cursor.cs
+++ Cursor.cs
@@ -6,6 +6,8 @@ using XTools;
 namespace XRepository {
     public class Cursor<T> : IEnumerable<T> where T : new() {
 
+        private static Cache<Type, Mirror> mirrorCache = new Cache<Type, Mirror>(t => new Mirror(t));
+
         // data and fetch are closely related.  data is essentially initialized
         // the the results of fetch whenever results need to be pulled.
         private IList<T> data;
@@ -113,6 +115,15 @@ namespace XRepository {
 
 
 
+        public Cursor<T> Sort(IDictionary<string, bool> sortBy) {
+            var sortByDictionary = new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase);
+            foreach (var column in sortBy.Keys)
+                sortByDictionary[column] = sortBy[column] ? 1 : -1;
+            return Sort(sortByDictionary);
+        } // end method
+
+
+
         public Cursor<T> Sort(IDictionary<string, int> sortBy) {
             data = null;
             CursorData.sort = sortBy;
@@ -130,8 +141,59 @@ namespace XRepository {
 
 
 
+        public Cursor<T> Sort(object sortBy) {
+            if (sortBy == null)
+                return Sort(new string[0]);
+
+            if (TryKnownSorts(sortBy))
+                return this;
+
+            var sortByMap = new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase);
+            var type = sortBy.GetType();
+            var mirror = mirrorCache[type];
+            foreach (var prop in mirror.GetProperties()) {
+                var value = prop.GetValue(sortBy, null);
+                if (value == null)
+                    continue;
+
+                var boolVal = value as bool?;
+                if (boolVal != null)
+                    value = boolVal.Value ? 1 : -1;
+                
+                var intVal = value as int?;
+                if (intVal != null)
+                    sortByMap[prop.Name] = intVal.Value;
+            } // end foreach
+            return Sort(sortByMap);
+        } // end method
+
+
+
         public Cursor<T> Sort(params string[] sortBy) {
             return Sort((IEnumerable<string>)sortBy);
+        } // end method
+
+
+
+        private bool TryKnownSorts(object sortBy)  {
+            var sortByStr = sortBy as string;
+            if (sortByStr != null)
+                Sort(new[] {sortByStr});
+            
+            var sortByStrs = sortBy as string[];
+            if (sortByStrs != null)
+                Sort(sortByStrs);
+            
+            var sortByBoolMap = sortBy as IDictionary<string, bool>;
+            if (sortByBoolMap != null)
+                Sort(sortByBoolMap);
+            
+            var sortByIntMap = sortBy as IDictionary<string, int>;
+            if (sortByIntMap != null)
+                Sort(sortByIntMap);
+
+            return sortByStr != null || sortByStrs != null ||
+                sortByBoolMap != null || sortByIntMap != null;
         } // end method
 
     } // end class
