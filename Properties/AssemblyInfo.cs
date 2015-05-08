015d607d933465ed2458fb4473ffe7cac59fcade
Aron Boyette <aron.boyette@xanotech.com>
2015-05-07 23:47:16 -0400

    Updated version from 0.8 to 0.9.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,5 +5,5 @@
 [assembly: AssemblyCopyright("© 2015 Xanotech LLC")]
 [assembly: AssemblyProduct("XRepository")]
 
-[assembly: AssemblyFileVersion("0.8")]
-[assembly: AssemblyVersion("0.8")]
+[assembly: AssemblyFileVersion("0.9")]
+[assembly: AssemblyVersion("0.9")]
