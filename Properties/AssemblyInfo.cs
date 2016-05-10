c7584e570fee7ec9b831b20eeb969869f74f2179
Aron Boyette <aron.boyette@xanotech.com>
2016-05-10 13:53:38 -0400

    Updated version from 0.11 to 0.12.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 
 [assembly: AssemblyTitle("XRepository: ORM Through Repository Pattern")]
 [assembly: AssemblyCompany("Xanotech LLC")]
-[assembly: AssemblyCopyright("© 2015 Xanotech LLC")]
+[assembly: AssemblyCopyright("© 2016 Xanotech LLC")]
 [assembly: AssemblyProduct("XRepository")]
 
 [assembly: AssemblyFileVersion("0.12")]
