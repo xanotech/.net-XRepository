745e33bdb3f7c44ec91f58e3b08ed1496da0d95d
Aron Boyette <aron.boyette@xanotech.com>
2017-06-21 21:02:07 -0400

    Set version to 0.13.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,8 +2,8 @@
 
 [assembly: AssemblyTitle("XRepository: ORM Through Repository Pattern")]
 [assembly: AssemblyCompany("Xanotech LLC")]
-[assembly: AssemblyCopyright("© 2016 Xanotech LLC")]
+[assembly: AssemblyCopyright("© 2017 Xanotech LLC")]
 [assembly: AssemblyProduct("XRepository")]
 
-[assembly: AssemblyFileVersion("0.12")]
-[assembly: AssemblyVersion("0.12")]
+[assembly: AssemblyFileVersion("0.13")]
+[assembly: AssemblyVersion("0.13")]
